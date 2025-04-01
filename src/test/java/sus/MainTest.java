6e662e9b54337009b2f27b68d8a9b0ea36b37882
Elena Ibanez <elenaaibzz@gmail.com>
2025-04-02 00:56:06 +0200

    added for loop

--- src/test/java/sus/MainTest.java
+++ src/test/java/sus/MainTest.java
@@ -7,30 +7,34 @@ import java.util.ArrayList;
 
 public class MainTest {
 
-    public BigInteger doStuff(int n){
-        System.out.println("Hello, World!");
-        if (n == 0) return BigInteger.ZERO;
-        if (n == 1) return BigInteger.ONE;
+    public BigInteger doStuff(int n) {
+        BigInteger result = BigInteger.ZERO;
 
-        BigInteger a = BigInteger.ZERO;
-        BigInteger b = BigInteger.ONE;
+        for (int j = 0; j < 5; j++) {
+            BigInteger a = BigInteger.ZERO;
+            BigInteger b = BigInteger.ONE;
+            ArrayList<BigInteger> list = new ArrayList<>();
+            BigInteger temp;
 
-        ArrayList<BigInteger> list = new ArrayList<>();
-        BigInteger temp;
+            if (n == 0) return BigInteger.ZERO;
+            if (n == 1) return BigInteger.ONE;
 
-        for (int i = 2; i <= n; i++) {
-            temp = b;
-            b = b.add(a);
-            a = temp;
-            list.add(b);
+            for (int i = 2; i <= n; i++) {
+                temp = b;
+                b = b.add(a);
+                a = temp;
+                list.add(b);
+            }
+            result = b;
         }
-        return b;
+
+        return result;
     }
 
     @Test
     public void calculateTest() {
         Main main = new Main();
-        main.calculate(1, 2);
-        BigInteger big = doStuff(30000);
+        main.calculate(1, 2);  
+        BigInteger big = doStuff(200000);
     }
 }
